33fa2eb29ed7b968f96b4a02f782c3f7507d47c5
ifratul36 <jannatrituwd@gmail.com>
2025-02-19 17:20:56 +0600

    project design set up

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -25,16 +25,7 @@
 //     setTheme(newTheme);
 //     document.documentElement.setAttribute('data-theme', newTheme);
 //     localStorage.setItem('theme', newTheme);
-//   };
-
-
-//   return (
-//     <div className={`navbar fixed z-50 w-full px-6 py-4 transition-all duration-300 ${theme === 'dark' ? 'bg-[#1c232b] text-white' : 'bg-[#eedede95] text-black'}`}>
-//       <div className="navbar-start">
-//         <div className="dropdown text-neutral-900">
-//           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-//             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#fff]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+//  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
 //             </svg>
 //           </div>
 //           <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
